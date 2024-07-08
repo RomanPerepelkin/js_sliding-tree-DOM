630e505401ecf4eb48e9ac47c94821d66f3980b9
Roman Perepelkin <perepelkinrs@gmail.com>
2024-07-08 10:36:22 +0300

    make build

---
+++ index.ec8c54bc.js
@@ -0,0 +1,2 @@
+var e=document.querySelector(".tree");document.querySelectorAll("li").forEach(function(e){var t=document.createElement("span");e.prepend(t),t.prepend(t.nextSibling)}),e.addEventListener("click",function(e){if("SPAN"===e.target.tagName){var t=e.target.nextSibling;"UL"===t.tagName&&(t.hidden=!t.hidden)}});
+//# sourceMappingURL=index.ec8c54bc.js.map
